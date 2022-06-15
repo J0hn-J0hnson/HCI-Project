1a9cb4bb291c11b0f09f97941456aa054e9a5732
J0hn-J0hnson <91310490+J0hn-J0hnson@users.noreply.github.com>
2022-06-15 08:52:07 +0700

    reg

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 const name = document.getElementById('name')
-const confirm_name = document.getElementById('confirm_name')
-const confirm_password = document.getElementById('confirm_password')
+const email = document.getElementById('confirm_name')
+const address = document.getElementById('address')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
+const check = document.getElementById('check')
 
 form.addEventListener('submit', (e) => {
   if (name.value === '' || name.value == null) {
     alert("Name is required!");
+  }else if(email.value === '' || email.value == null){
+    alert("Email is required!");
   }else if (password.value === '' || password.value == null) {
     alert("Password is required!");
-  }else if (confirm_name.value === '' || confirm_name.value == null) {
-    alert("Confirm Name is required!");
-  }else if (confirm_password.value === '' || confirm_password.value == null) {
-    alert("Confirm Password is required!");
-  }else if (name.value != confirm_name.value){
-    alert("Name and Confirm name must be the same!")
-  }else if (password.value != confirm_password.value){
-    alert("Password and Confrim Password must be the same!")
+  }else if (address.value === '' || address.value == null){
+    alert("Address is required!");
+  }else if (check.checked === 0){
+    alert("agree to the terms and conditions!")
   }else{
     alert("Account registered!")
   }
